a8ca7216b9106769ae1297463d84ac4eb00816aa
Kun Devanny <kun.devanny@neulion.com>
2017-09-14 16:11:11 +0100

    Add bitscape, kebab functions to aid with bitbucket specific formatting

--- main.go
+++ main.go
@@ -60,6 +60,8 @@ var (
 		"base":       path.Base,
 		"md":         mdFunc,
 		"pre":        preFunc,
+		"kebab":      kebabFunc,
+		"bitscape":   bitscapeFunc, //Escape [] for bitbucket confusion
 	}
 )
 
@@ -88,6 +90,19 @@ func readTemplate(name, data string) *template.Template {
 		log.Fatal("readTemplate: ", err)
 	}
 	return t
+}
+
+func kebabFunc(text string) string {
+	s := strings.Replace(strings.ToLower(text), " ", "-", -1)
+	s = strings.Replace(s, ".", "-", -1)
+	s = strings.Replace(s, "\\*", "42", -1)
+	return s
+}
+
+func bitscapeFunc(text string) string {
+	s := strings.Replace(text, "[", "\\[", -1)
+	s = strings.Replace(s, "]", "\\]", -1)
+	return s
 }
 
 func main() {
